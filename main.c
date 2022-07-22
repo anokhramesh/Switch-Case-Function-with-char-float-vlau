a32905ad82b9b504cc50620eedf6369f52aed2ba
Anokhautomation <67797819+anokhramesh@users.noreply.github.com>
2022-07-22 20:59:37 +0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+// Add two integer Numbers in C program
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+
+{
+int num1,num2,sum;// variable num1, num2 and sum are int data type
+printf("Enter the value of num1\n");// print a command
+scanf("%d",& num1);// inputing the value of num1
+printf("Enter the value of num2\n");// print a command
+scanf("%d",& num2);// inputing the value of num2
+printf("sum is = %d\n",num1+num2);// printing the sum of num1 + num2
+
+return 0;
+}
